8ffc62f555b4674f65f7146f81757cbd395da709
Nico <Nico@NICO>
2023-12-01 10:26:42 +0100

    HOY APROBARE EL EXAMEN

---
+++ hoyeseldia/src/hoyeseldia/hoyeseldia.java
@@ -0,0 +1,12 @@
+package hoyeseldia;
+
+public class hoyeseldia {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola, hoy es el dia");
+
+	}
+
+}
